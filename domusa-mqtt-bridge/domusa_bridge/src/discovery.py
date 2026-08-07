24fc2769791179daa6057ad2582ddcd4848d5593
Donzaffi <47207943+Donzaffi@users.noreply.github.com>
2026-08-07 08:53:49 +0200

    Update discovery.py

--- domusa-mqtt-bridge/domusa_bridge/src/discovery.py
+++ domusa-mqtt-bridge/domusa_bridge/src/discovery.py
@@ -1,11 +1,9 @@
 import json
-from i18n import I18n
 
 class Discovery:
     def __init__(self, mqtt, device, lang="de"):
         self.mqtt = mqtt
         self.device = device
-        self.i18n = I18n(lang)
 
     async def publish(self):
         cid = self.device["id"]
@@ -16,48 +14,293 @@ class Discovery:
             "model": "HTEC Pro 12"
         }
 
-        sensors = [
-            {"name": self.i18n.t("sensor_evap"), "uid": "s_evap", "key": "s_evap_c00", "unit": "°C", "class": "temperature", "icon": "mdi:thermometer"},
-            {"name": self.i18n.t("sensor_discharge"), "uid": "s_discharge", "key": "s_discharge_c01", "unit": "°C", "class": "temperature", "icon": "mdi:thermometer"},
-            {"name": self.i18n.t("sensor_suction"), "uid": "s_suction", "key": "s_suction_c03", "unit": "°C", "class": "temperature", "icon": "mdi:thermometer"},
-            {"name": self.i18n.t("sensor_condens"), "uid": "s_condens", "key": "s_condens_c06", "unit": "°C", "class": "temperature", "icon": "mdi:thermometer"},
-            {"name": self.i18n.t("sensor_heat_set"), "uid": "st_heat", "key": "st_c_p02", "unit": "°C", "class": "temperature", "icon": "mdi:radiator"},
-            {"name": self.i18n.t("sensor_cool_set"), "uid": "st_cool", "key": "st_f_p03", "unit": "°C", "class": "temperature", "icon": "mdi:snowflake"},
-            {"name": self.i18n.t("sensor_active_heat"), "uid": "st_active_heat", "key": "st_activa_c_f", "unit": "°C", "class": "temperature", "icon": "mdi:radiator"},
-            {"name": self.i18n.t("sensor_active_acs"), "uid": "st_active_acs", "key": "st_activa_acs", "unit": "°C", "class": "temperature", "icon": "mdi:water-thermometer"},
-            {"name": self.i18n.t("sensor_ipm"), "uid": "ipm_temp", "key": "s_ipm_c22", "unit": "°C", "class": "temperature", "icon": "mdi:thermometer"},
-            {"name": self.i18n.t("sensor_t6"), "uid": "t6_temp", "key": "s_t6_c25", "unit": "°C", "class": "temperature", "icon": "mdi:thermometer"},
-            {"name": self.i18n.t("sensor_zone_ext"), "uid": "zone_ext_temp", "key": "s_zonaext_c75", "unit": "°C", "class": "temperature", "icon": "mdi:thermometer"},
-            {"name": self.i18n.t("sensor_delta"), "uid": "delta_t", "key": "dt_hp_c11", "unit": "°C", "class": "temperature", "icon": "mdi:thermometer"},
-            {"name": self.i18n.t("sensor_phase"), "uid": "phase_current", "key": "curr_ph_comp", "unit": "A", "class": "current", "icon": "mdi:current-ac"},
-            {"name": self.i18n.t("sensor_volt"), "uid": "volt_dc", "key": "volt_dc_c24", "unit": "V", "class": "voltage", "icon": "mdi:flash"},
-            {"name": self.i18n.t("sensor_fan1"), "uid": "fan1_rpm", "key": "rpm_vent1_c16", "unit": "rpm", "class": None, "icon": "mdi:fan"},
-            {"name": self.i18n.t("sensor_fan2"), "uid": "fan2_rpm", "key": "rpm_vent2_c17", "unit": "rpm", "class": None, "icon": "mdi:fan"},
-            {"name": self.i18n.t("sensor_eev"), "uid": "eev_position", "key": "eev_c18", "unit": "steps", "class": None, "icon": "mdi:valve"},
-            {"name": self.i18n.t("sensor_pwm"), "uid": "pwm", "key": "pwm_c1_c51", "unit": "%", "class": None, "icon": "mdi:percent"},
-            {"name": self.i18n.t("sensor_power"), "uid": "thermal_power", "key": "s_q_c10", "unit": "kW", "class": "power", "icon": "mdi:flash-circle"},
-            {
-                "name": self.i18n.t("sensor_state"), "uid": "operation_state", "key": "estado_func_c52", "unit": None, "class": None, "icon": "mdi:state-machine",
-                "template": "{% set mapper = {0: 'Standby', 1: 'Warmwasser', 2: 'Heizen', 3: 'Warmwasser+Heizen', 3: 'Kühlen'} %}{{ mapper.get(value_json.estado_func_c52 | int, 'Unbekannt') }}"
-            },
-            #{"name": self.i18n.t("sensor_state"), "uid": "operation_state", "key": "estado_func_c52", "unit": None, "class": None, "icon": "mdi:state-machine"},
-            {"name": self.i18n.t("sensor_mode"), "uid": "hp_mode", "key": "m_hp_p01", "unit": None, "class": None, "icon": "mdi:cog"},
-            {"name": self.i18n.t("sensor_freq"), "uid": "grid_frequency", "key": "e_freq", "unit": "Hz", "class": "frequency", "icon": "mdi:sine-wave"},
-            {"name": self.i18n.t("sensor_zone1"), "uid": "zone1_heat_setpoint", "key": "st_zona1_c_p158", "unit": "°C", "class": "temperature", "icon": "mdi:radiator"},
-            {"name": self.i18n.t("sensor_acs_temp"), "uid": "s_acs", "key": "s_acs_c09", "unit": "°C", "class": "temperature", "icon": "mdi:water-thermometer"}
-        ]
+       sensors = [
 
-        for s in sensors:
+    #
+    # Temperaturen
+    #
+    {
+        "uid": "outside_temperature",
+        "name": "Außentemperatur",
+        "key": "s_ext_c02",
+        "device_class": "temperature",
+        "unit": "°C",
+        "icon": "mdi:thermometer"
+    },
+    {
+        "uid": "evaporator_temperature",
+        "name": "Verdampfer",
+        "key": "s_evap_c00",
+        "device_class": "temperature",
+        "unit": "°C",
+        "icon": "mdi:thermometer"
+    },
+    {
+        "uid": "discharge_temperature",
+        "name": "Verdichterausgang",
+        "key": "s_discharge_c01",
+        "device_class": "temperature",
+        "unit": "°C",
+        "icon": "mdi:thermometer"
+    },
+    {
+        "uid": "suction_temperature",
+        "name": "Sauggas",
+        "key": "s_suction_c03",
+        "device_class": "temperature",
+        "unit": "°C",
+        "icon": "mdi:thermometer"
+    },
+    {
+        "uid": "condenser_temperature",
+        "name": "Verflüssiger",
+        "key": "s_condens_c06",
+        "device_class": "temperature",
+        "unit": "°C",
+        "icon": "mdi:thermometer"
+    },
+    {
+        "uid": "flow_temperature",
+        "name": "Vorlauf",
+        "key": "s_ida_hp_c08",
+        "device_class": "temperature",
+        "unit": "°C",
+        "icon": "mdi:radiator"
+    },
+    {
+        "uid": "return_temperature",
+        "name": "Rücklauf",
+        "key": "s_ret_hp_c07",
+        "device_class": "temperature",
+        "unit": "°C",
+        "icon": "mdi:radiator"
+    },
+    {
+        "uid": "dhw_temperature",
+        "name": "Warmwasser",
+        "key": "s_acs_c09",
+        "device_class": "temperature",
+        "unit": "°C",
+        "icon": "mdi:water-thermometer"
+    },
+    {
+        "uid": "dhw_target_temperature",
+        "name": "Warmwasser Soll",
+        "key": "st_activa_acs",
+        "device_class": "temperature",
+        "unit": "°C",
+        "icon": "mdi:water-thermometer"
+    },
+    {
+        "uid": "heating_target_temperature",
+        "name": "Heizung Soll",
+        "key": "st_activa_c_f",
+        "device_class": "temperature",
+        "unit": "°C",
+        "icon": "mdi:radiator"
+    },
+    {
+        "uid": "delta_temperature",
+        "name": "Delta T",
+        "key": "dt_hp_c11",
+        "device_class": "temperature",
+        "unit": "°C",
+        "icon": "mdi:thermometer"
+    },
+    {
+        "uid": "ipm_temperature",
+        "name": "IPM",
+        "key": "s_ipm_c22",
+        "device_class": "temperature",
+        "unit": "°C",
+        "icon": "mdi:thermometer",
+        "entity_category": "diagnostic"
+    },
+    {
+        "uid": "t6_temperature",
+        "name": "T6",
+        "key": "s_t6_c25",
+        "device_class": "temperature",
+        "unit": "°C",
+        "icon": "mdi:thermometer",
+        "entity_category": "diagnostic"
+    },
+
+    #
+    # Elektrik
+    #
+    {
+        "uid": "ac_voltage",
+        "name": "AC Spannung",
+        "key": "volt_ac_c23",
+        "device_class": "voltage",
+        "unit": "V",
+        "icon": "mdi:flash"
+    },
+    {
+        "uid": "dc_voltage",
+        "name": "DC Spannung",
+        "key": "volt_dc_c24",
+        "device_class": "voltage",
+        "unit": "V",
+        "icon": "mdi:flash"
+    },
+    {
+        "uid": "compressor_current",
+        "name": "Kompressorstrom",
+        "key": "curr_comp_c21",
+        "device_class": "current",
+        "unit": "A",
+        "icon": "mdi:current-ac"
+    },
+    {
+        "uid": "phase_current",
+        "name": "Phasenstrom",
+        "key": "curr_ph_comp",
+        "device_class": "current",
+        "unit": "A",
+        "icon": "mdi:current-ac"
+    },
+
+    #
+    # Kältekreis
+    #
+    {
+        "uid": "high_pressure",
+        "name": "Hochdruck",
+        "key": "s_pralta_c13",
+        "unit": "MPa",
+        "icon": "mdi:gauge",
+        "entity_category": "diagnostic"
+    },
+    {
+        "uid": "low_pressure",
+        "name": "Niederdruck",
+        "key": "s_prbaja_c14",
+        "unit": "MPa",
+        "icon": "mdi:gauge",
+        "entity_category": "diagnostic"
+    },
+    {
+        "uid": "compressor_frequency",
+        "name": "Kompressorfrequenz",
+        "key": "freq_c15",
+        "device_class": "frequency",
+        "unit": "Hz",
+        "icon": "mdi:sine-wave"
+    },
+    {
+        "uid": "eev_position",
+        "name": "EEV",
+        "key": "eev_c18",
+        "unit": "steps",
+        "icon": "mdi:valve",
+        "entity_category": "diagnostic"
+    },
+    {
+        "uid": "pwm",
+        "name": "PWM",
+        "key": "pwm_c1_c51",
+        "unit": "%",
+        "icon": "mdi:percent",
+        "entity_category": "diagnostic"
+    },
+
+    #
+    # Lüfter
+    #
+    {
+        "uid": "fan1_rpm",
+        "name": "Lüfter 1",
+        "key": "rpm_vent1_c16",
+        "unit": "rpm",
+        "icon": "mdi:fan",
+        "entity_category": "diagnostic"
+    },
+    {
+        "uid": "fan2_rpm",
+        "name": "Lüfter 2",
+        "key": "rpm_vent2_c17",
+        "unit": "rpm",
+        "icon": "mdi:fan",
+        "entity_category": "diagnostic"
+    },
+
+    #
+    # Betrieb
+    #
+    {
+        "uid": "operation_mode",
+        "name": "Betriebsmodus",
+        "key": "m_hp_p01",
+        "icon": "mdi:cog"
+    },
+    {
+        "uid": "operation_state",
+        "name": "Betriebszustand",
+        "key": "estado_func_c52",
+        "icon": "mdi:state-machine",
+        "value_template":
+            "{% set map={0:'Standby',1:'Warmwasser',2:'Heizen',3:'Warmwasser + Heizen',4:'Kühlen'} %}"
+            "{{ map.get(value_json.estado_func_c52|int,'Unbekannt') }}"
+    },
+    {
+        "uid": "grid_frequency",
+        "name": "Netzfrequenz",
+        "key": "e_freq",
+        "device_class": "frequency",
+        "unit": "Hz",
+        "icon": "mdi:sine-wave"
+    },
+    {
+        "uid": "alarm",
+        "name": "Alarm",
+        "key": "alarma",
+        "icon": "mdi:alert"
+    },
+    {
+        "uid": "sub_alarm",
+        "name": "Unteralarm",
+        "key": "sub_alarma",
+        "icon": "mdi:alert-circle"
+    }
+]
+
+        for sensor in sensors:
+
             payload = {
-                "name": f"Domusa {s['name']}",
-                "unique_id": f"domusa_{cid}_{s['uid']}",
+                "name": f"Domusa {sensor['name']}",
+                "unique_id": f"domusa_{cid}_{sensor['uid']}",
                 "device": dev_info,
                 "state_topic": f"domusa/{cid}/status",
-                "value_template": f"{{{{ value_json.{s['key']} }}}}",
-                "unit_of_measurement": s['unit'],
-                "device_class": s['class'],
-                "state_class": "measurement" if s['unit'] else None,
-                "icon": s.get('icon')
+                "value_template": sensor.get(
+                    "value_template",
+                    f"{{{{ value_json.{sensor['key']} }}}}"
+                ),
+                "icon": sensor.get("icon")
             }
-            await self.mqtt.client.publish(f"homeassistant/sensor/domusa_{cid}_{s['uid']}/config", json.dumps(payload), retain=True)
+
+            if "unit" in sensor:
+                payload["unit_of_measurement"] = sensor["unit"]
+
+            if "device_class" in sensor:
+                payload["device_class"] = sensor["device_class"]
+
+            #
+            # state_class nur für numerische Sensoren
+            #
+            if sensor.get("unit") and sensor.get("device_class") not in (
+                "enum",
+                "timestamp"
+            ):
+                payload["state_class"] = "measurement"
+
+            if "entity_category" in sensor:
+                payload["entity_category"] = sensor["entity_category"]
+
+            await self.mqtt.client.publish(
+                f"homeassistant/sensor/domusa_{cid}_{sensor['uid']}/config",
+                json.dumps(payload),
+                retain=True
+            )
 
